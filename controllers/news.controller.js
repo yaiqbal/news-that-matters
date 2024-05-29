07201ae73b2c0264bff6fe3536897fb1787c9cdc
Yunus <yunus_ahmed@yahoo.com>
2024-05-29 13:27:54 +0100

    This commit completes Task 4 - GET /api/articles/:article_id and implements review comments from Task 3 (https://github.com/yaiqbal/news-that-matters/pull/3#discussion_r1618590931)

--- controllers/news.controller.js
+++ controllers/news.controller.js
@@ -1,4 +1,4 @@
-const { selectTopics } = require("../models/news.model");
+const { selectTopics, selectArticleById } = require("../models/news.model");
 const endpointData =  require(`${__dirname}/../endpoints.json`)
 
 exports.getTopics = (req, res, next) => {
@@ -10,4 +10,20 @@ exports.getTopics = (req, res, next) => {
 
 exports.getEndpoints = (req, res, next) => {
   return res.status(200).send({endpointData});
+};
+
+exports.getArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  selectArticleById(article_id)
+    .then((articles) => {
+      if (!articles.length) {
+        return res.status(404).send({ msg: 'Article does not exist' });
+      }
+      res.status(200).send({ articles });
+    })
+    .catch((err) => {
+      if(err.code === "22P02") {
+        res.status(400).send({ msg: 'Bad request' });
+      }
+    })
 };
